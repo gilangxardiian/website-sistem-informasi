c141db10b4b2ba2f2609f24ef5ce8f610ec6d714
Muhamad Gilang Ardian <gilangardian637@gmail.com>
2023-08-09 07:55:33 +0700

    Menamahkan file style-me.css dan update file fungsi

--- assets/functions/functions.php
+++ assets/functions/functions.php
@@ -16,3 +16,58 @@ function koneksi()
     // kembalikan variabel conn
     return $conn;
 }
+
+// fungsi mengamil data dari dataase
+function query($query)
+{
+    // ambil fungsi koneksi
+    $koneksi = koneksi();
+
+    // lakukan kueri pencarian data
+    $result = mysqli_query($koneksi, $query);
+    // array penampungan
+    $rows = [];
+
+    // selagi ada data array assosiatve
+    while ($row = mysqli_fetch_assoc($result)) {
+        // maka masukan data tersebut ke dalam array penampungan
+        $rows[] = $row;
+    }
+
+    // kembalikan array penampungan
+    return $rows;
+}
+
+function masuk($username, $password)
+{
+    // ambil fungsi koneksi
+    $koneksi = koneksi();
+
+    // lakukan kueri untuk menemukan username yang sesuai di database
+    $result = mysqli_query($koneksi, "SELECT * FROM pengguna WHERE username = '$username'");
+
+    // mengecek username ketemu atau tidak
+    if (mysqli_num_rows($result) > 0) {
+        // jika ketemu cek passwordnya sesuai atau tidak
+        $row = mysqli_fetch_assoc($result);
+
+        // lakukan pemecahan enkripsi
+        if (password_verify($password, $row["password"])) {
+            // mengirimkan id menggunakan session
+            $_SESSION["id"] = $row["idPengguna"];
+            // mengirimkan info login menggunakan session
+            $_SESSION["login"] = true;
+
+            // jika password sesuai maka arahkan
+            $arahkan = header("Location: ../../index.php");
+            return $arahkan;
+            exit;
+        } else {
+            // jika tidak sesuai maka set variable error sama dengan satu
+            global $error;
+            $error = true;
+
+            return $error;
+        }
+    }
+}
